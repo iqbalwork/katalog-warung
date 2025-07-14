acbab75a78743c6e212e40c5087190ad73cde335
iqbalwork <work.iqbalfauzi@gmail.com>
2025-07-14 20:23:57 +0700

    Update: Libraries
    Signed-off-by: iqbalwork <work.iqbalfauzi@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -49,6 +51,27 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // Room Persistence
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.ksp)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.pagination)
+    testImplementation(libs.androidx.room.testing)
+
+    // Coroutines & Flow
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.ksp)
+
+    // Coil Image Loader
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
